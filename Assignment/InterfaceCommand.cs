937c0b9cdad326c521ff7538b7aed4ef8b3a4d90
gourav263587 <133707861+gourav263587@users.noreply.github.com>
2023-06-09 22:20:44 -0400

    Update InterfaceCommand.cs

--- Assignment/InterfaceCommand.cs
+++ Assignment/InterfaceCommand.cs
@@ -1,6 +1,37 @@
-﻿namespace Assignment.InterfaceCommand;
+namespace Assignment.InterfaceCommand;
 
-public interface RobotCommand
+// IRobotCommand interface declares the Run method for executing a robot command.
+public interface IRobotCommand
 {
     void Run(Robot robot); // Interfaces are public and abstract by default.
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = false;
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = true;
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X--; }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X++; }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y--; }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y++; }
 }
