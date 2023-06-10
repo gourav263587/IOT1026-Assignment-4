d4f440eb1bc2bbdf1d95c4d6e3dc1e22ad60948c
gourav263587 <133707861+gourav263587@users.noreply.github.com>
2023-06-09 22:22:22 -0400

    Update Robot.cs

--- Assignment/Robot.cs
+++ Assignment/Robot.cs
@@ -1,4 +1,4 @@
-﻿// Change to 'using Assignment.InterfaceCommand' when you are ready to test your interface implementation
+// Change to 'using Assignment.InterfaceCommand' when you are ready to test your interface implementation
 using Assignment.InterfaceCommand;
 
 namespace Assignment;
@@ -6,21 +6,73 @@ namespace Assignment;
 public class Robot
 {
     // These are properties, you can replace these with traditional getters/setters if you prefer.
+    /// <summary>
+    /// Gets the number of commands loaded in the robot.
+    /// </summary>
     public int NumCommands { get; }
 
-    // These properties are not good! The setter allows
-    // us to move the robot even if it off
-    public int X { get; set; }
-    public int Y { get; set; }
+    /// <summary>
+    /// Gets or sets the X-coordinate of the robot.
+    /// </summary>
+    private int _x;
+    /// <summary>
+    /// Gets or sets the X coordinate of the robot's position.
+    /// </summary>
+    /// <value>The X coordinate value.</value>
+    /// <exception cref="InvalidOperationException">Thrown when the robot is not powered on.</exception>
+    public int X
+    {
+        get { return _x; }
+        set
+        {
+            if (IsPowered)
+                _x = value;
+            else
+                throw new InvalidOperationException("Cannot move the robot when it is not powered on.");
+        }
+    }
+    
+    /// <summary>
+    /// Gets or sets the Y-coordinate of the robot.
+    /// </summary>
+    /// <summary>
+    /// Represents the Y coordinate of the robot's position.
+    /// </summary>
+    private int _y;
+
+    /// <summary>
+    /// Gets or sets the Y coordinate of the robot's position.
+    /// </summary>
+    /// <value>The Y coordinate value.</value>
+    /// <exception cref="InvalidOperationException">Thrown when the robot is not powered on.</exception>
+    public int Y
+    {
+        get { return _y; }
+        set
+        {
+            if (IsPowered)
+                _y = value;
+            else
+                throw new InvalidOperationException("Cannot move the robot when it is not powered on.");
+        }
+    }
+    
+    /// <summary>
+    /// Gets or sets a value indicating whether the robot is powered on.
+    /// </summary>
     public bool IsPowered { get; set; }
 
     private const int DefaultCommands = 6;
     // An array is not the preferred data structure here.
     // You will get bonus marks if you replace the array with the preferred data structure
     // Hint: It is NOT a list either,
-    private readonly RobotCommand[] _commands;
+    private readonly IRobotCommand[] _commands;
     private int _commandsLoaded = 0;
 
+    /// <summary>
+    /// Returns a string that represents the current robot coordinates and power status.
+    /// </summary>        
+    /// <returns>A string representation of the robot.</returns>
     public override string ToString()
     {
         return $"[{X} {Y} {IsPowered}]";
@@ -38,12 +90,12 @@ public class Robot
     /// <param name="numCommands">The maximum number of commands the robot can store</param>
     public Robot(int numCommands)
     {
-        _commands = new RobotCommand[numCommands];
+        _commands = new IRobotCommand[numCommands];
         NumCommands = numCommands;
     }
 
     /// <summary>
-    ///
+    /// 
     /// </summary>
     public void Run()
     {
@@ -55,15 +107,15 @@ public class Robot
     }
 
     /// <summary>
-    ///
+    /// Returns false if commandloaded is more than numcommands and if not will return true
     /// </summary>
     /// <param name="command"></param>
     /// <returns></returns>
-    public bool LoadCommand(RobotCommand command)
+    public bool LoadCommand(IRobotCommand command)
     {
         if (_commandsLoaded >= NumCommands)
             return false;
         _commands[_commandsLoaded++] = command;
-        return true;
-    }
+        return true;
+    }
 }
