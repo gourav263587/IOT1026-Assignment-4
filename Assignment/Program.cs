9ae2ed5cb9ce6bf97058682fe5179c98c6193473
gourav263587 <133707861+gourav263587@users.noreply.github.com>
2023-06-09 22:21:38 -0400

    Update Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,11 +1,69 @@
-﻿namespace Assignment
+namespace Assignment.InterfaceCommand 
 {
-    static class Program
+    public class RobotTester
     {
-        static void Main()
+        public void Run()
         {
-            System.Console.WriteLine("hello world");
-            // Run your RobotTester class here -> RobotTester.TestRobot()
+            Robot robot = new Robot();
+
+            Console.WriteLine("Enter command strings (separated by commas):");
+            string input = Console.ReadLine();
+
+            string[] commandStrings = input.Split(',');
+            foreach (string commandString in commandStrings)
+            {
+                IRobotCommand command = CreateCommand(commandString);
+                if (command != null)
+                {
+                    robot.LoadCommand(command);
+                }
+                if (commandString.Trim().ToLower() == "off")
+                {
+                    break;
+                }
+            }
+
+            robot.Run();
         }
+
+         private IRobotCommand CreateCommand(string commandString)
+         {
+            commandString = commandString.Trim().ToLower();
+
+            switch (commandString)
+            {
+                case "on":
+                    return new OnCommand();
+                case "off":
+                    return new OffCommand();
+                case "west":
+                    return new WestCommand();
+                case "east":
+                    return new EastCommand();
+                case "south":
+                    return new SouthCommand();
+                case "north":
+                    return new NorthCommand();
+                default:
+                    Console.WriteLine($"Invalid command: {commandString}");
+                    return null;
+            }
+         }
     }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Give 6 commands to the robot, possible commands are:");
+            Console.WriteLine("on");
+            Console.WriteLine("off");
+            Console.WriteLine("north");
+            Console.WriteLine("south");
+            Console.WriteLine("east");
+            Console.WriteLine("west");
+            RobotTester tester = new RobotTester();
+            tester.Run();
+        }
+    }
 }
